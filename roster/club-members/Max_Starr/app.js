41dcc71fdda17f8e22b76651cd6da9f66b5bdd69
Max Starr <manmon42@gmail.com>
2015-05-30 22:06:07 +0000

    Started on a nav thing

--- roster/club-members/Max_Starr/app.js
+++ roster/club-members/Max_Starr/app.js
@@ -3,15 +3,18 @@
  */
 angular.module('myPage', [])
 
+    .directive('myNav', function () {
+    'use strict';
+    return {
+      restrict: 'E',
+      replace: 'true',
+      templateUrl: './data/nav.html'
+    };
+  })
+
     .controller('myController', ['$scope', '$http', function ($scope, $http) {
         'use strict';
-        $http.get('data/members.json').
-            success(function(data, status, headers, config) {
-                $scope.members = data;
-            }).
-            error(function(data, status, headers, config) {
-                console.log(status);
-            });
+
         $scope.author = 'Max Starr';
 
     }]);
