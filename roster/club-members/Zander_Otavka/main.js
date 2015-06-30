44c7c374108ec427b67741efbc99664adfe70635
Zander Otavka <zotavka@gmail.com>
2015-06-30 08:27:27 -0700

    Improve header slightly.

--- roster/club-members/Zander_Otavka/main.js
+++ roster/club-members/Zander_Otavka/main.js
@@ -4,19 +4,20 @@
   var mainImageEnd = document.querySelector("a#mainImageEnd");
   var titleText = document.querySelector("#titleText");
 
+  parallaxContainer.addEventListener("scroll", SmoothA.updateHash);
+  SmoothA.updateHash();
+
   parallaxContainer.addEventListener("scroll", function() {
     if (parallaxContainer.scrollTop >= mainImageEnd.getTop()) {
-      console.log(titleText.getAttribute('hidden'));
       if (titleText.getAttribute("hidden") !== null) {
         titleText.removeAttribute("hidden");
         header.classList.add("dark");
       }
     } else {
-      console.log(titleText.getAttribute('hidden'));
       if (titleText.getAttribute("hidden") === null) {
         titleText.setAttribute("hidden", "");
         header.classList.remove("dark");
       }
     }
-  })
+  });
 }());
