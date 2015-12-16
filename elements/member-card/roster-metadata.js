35ac407f24465bb59328dba7cbdead9f318fbb82
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2015-12-15 20:15:06 -0800

    Update 2015-12-16T04:12:35.714Z

---
+++ elements/member-card/roster-metadata.js
@@ -0,0 +1,114 @@
+window.ROSTER_METADATA = {
+  Abigail_Virgo: {
+    readableName: "Abigail Virgo",
+    blurb: null,
+    config: {
+      uri: "/roster/Abigail_Virgo"
+    }
+  },
+  Connor_Heffernan: {
+    readableName: "Connor Heffernan",
+    blurb: "I have little experience with Coding and am a History Nerd\n",
+    config: {
+      uri: "NachosPug.github.io"
+    }
+  },
+  Duncan_Grubbs: {
+    readableName: "Duncan Grubbs",
+    blurb: "Duncan is an aspiring programmer with knowledge in Java, Python\nand web languages.",
+    config: {
+      uri: "http://duncangrubbs.github.io"
+    }
+  },
+  Eleanor_Bemis: {
+    readableName: "Eleanor Bemis",
+    blurb: "Eleanor Bemis is a member of the Drake Developer's Club. She is new to coding and super\nexcited to learn!\n",
+    config: {
+      uri: "/roster/Eleanor_Bemis"
+    }
+  },
+  Gwyn_Gordon: {
+    readableName: "Gwyn Gordon",
+    blurb: "Hey guys it's Nicole... jkjk its Gwyn ;) I like cats and fashion hmu. I'm a 9th grader and new to coding and Drake High School.\n",
+    config: {
+      uri: "/roster/Gwyn_Gordon"
+    }
+  },
+  Jessica_Gerwin: {
+    readableName: "Jessica Gerwin",
+    blurb: "Jessica Gerwin is the President of the Developer's Club. Avid coffee enthusiast, tennis player and coder.\n",
+    config: {
+      uri: "/roster/Jessica_Gerwin"
+    }
+  },
+  Kira_Kargard: {
+    readableName: "Kira Kargard",
+    blurb: "Hi my name is Kira. I am in 11th grade at Drake High School",
+    config: {
+      uri: "/roster/Kira_Kargard"
+    }
+  },
+  Kylie_Jones: {
+    readableName: "Kylie Jones",
+    blurb: "I am new to coding this year as a junior, but have some expierence\nof working with websites from Galileo.\n",
+    config: {
+      uri: "/roster/Kylie_Jones"
+    }
+  },
+  Leo_Krashanoff: {
+    readableName: "Leo Krashanoff",
+    blurb: "Leo is a hobbyist coder with a basic knowledge of nearly all coding languages.  Squirtle is his favorite pokemon.\n",
+    config: {
+      uri: "http://krashanoff.github.io"
+    }
+  },
+  Luke_Martel: {
+    readableName: "Luke Martel",
+    blurb: "Luke is a young programmer looking to further develop his knowledge in Java,\nweb languages, and the command line.   \n",
+    config: {
+      uri: "http://lucasmartel.github.io"
+    }
+  },
+  Max_Starr: {
+    readableName: "Max Starr",
+    blurb: "Max is a fun loving character with a basic knowledge of many different things.",
+    config: {
+      uri: "http://manmon42.github.io"
+    }
+  },
+  Miles_Krick: {
+    readableName: "Miles Krick",
+    blurb: "I am new at coding and a Nintendo nerd.\n",
+    config: {
+      uri: "Pierocket.github.io"
+    }
+  },
+  Noah_Marty: {
+    readableName: "Noah Marty",
+    blurb: "Noah Marty is not very good a coding, but aspires to be better. What he lacks in skill, he makes up for in swag.\n",
+    config: {
+      uri: "/roster/Noah_Marty"
+    }
+  },
+  Sebastian_Boyd: {
+    readableName: "Sebastian Boyd",
+    blurb: "Sebastian Boyd is the CEO of SB Technology Holdings International. He likes to code Python and the web.\n",
+    config: {
+      uri: "/roster/Sebastian_Boyd"
+    }
+  },
+  Sophia_Lin: {
+    readableName: "Sophia Lin",
+    blurb: "I'm new to coding and don't have much experience with complex technology, but I'm very excited to start learning.\n",
+    config: {
+      uri: "/roster/Sophia_Lin"
+    }
+  },
+  Zander_Otavka: {
+    readableName: "Zander Otavka",
+    blurb: "Zander is the Vice President of the Club.  He is experienced in Java, Python, C++, and Web Languages.\n",
+    config: {
+      uri: "/roster/Zander_Otavka"
+    }
+  }
+};
---
+++ elements/responsive-img/image-index.js
@@ -0,0 +1,321 @@
+window.IMAGE_INDEX = {
+  "/images/data-center.jpg": {
+    resolutions: [
+      "1250w",
+      "2500w",
+      "833w"
+    ],
+    aspectRatio: {
+      width: 625,
+      height: 417
+    }
+  },
+  "/images/data-center-2.jpg": {
+    resolutions: [
+      "1000w",
+      "1500w",
+      "3000w"
+    ],
+    aspectRatio: {
+      width: 1000,
+      height: 667
+    }
+  },
+  "/images/data-center-2-blurred.jpg": {
+    resolutions: [
+      "1000w",
+      "1500w",
+      "3000w"
+    ],
+    aspectRatio: {
+      width: 1000,
+      height: 667
+    }
+  },
+  "/images/data-center-2-low-res.jpg": {
+    resolutions: [
+      "100w",
+      "150w",
+      "300w"
+    ],
+    aspectRatio: {
+      width: 100,
+      height: 67
+    }
+  },
+  "/images/default-profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/images/java-logo.jpg": {
+    resolutions: [
+      "160w",
+      "240w",
+      "480w"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/images/js-logo.jpg": {
+    resolutions: [
+      "160w",
+      "53w",
+      "80w"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/images/resource-codecademy.jpg": {
+    resolutions: [
+      "133w",
+      "200w",
+      "400w"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/images/resource-mdn-javascript-reference.jpg": {
+    resolutions: [
+      "200w",
+      "300w",
+      "600w"
+    ],
+    aspectRatio: {
+      width: 25,
+      height: 22
+    }
+  },
+  "/images/which-language.png": {
+    resolutions: [
+      "1000w",
+      "2000w",
+      "667w"
+    ],
+    aspectRatio: {
+      width: 200,
+      height: 221
+    }
+  },
+  "/roster/Connor_Heffernan/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Duncan_Grubbs/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Eleanor_Bemis/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Gwyn_Gordon/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Jessica_Gerwin/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Jessica_Gerwin/purplebridge.jpeg": {
+    resolutions: [
+      "1212w",
+      "2423w",
+      "808w"
+    ],
+    aspectRatio: {
+      width: 1212,
+      height: 1189
+    }
+  },
+  "/roster/Kira_Kargard/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Kylie_Jones/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Kylie_Jones/sunset.jpg": {
+    resolutions: [
+      "245w",
+      "368w",
+      "736w"
+    ],
+    aspectRatio: {
+      width: 245,
+      height: 153
+    }
+  },
+  "/roster/Leo_Krashanoff/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Luke_Martel/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Max_Starr/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Miles_Krick/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Sebastian_Boyd/fish.jpg": {
+    resolutions: [
+      "1824w",
+      "2736w",
+      "5472w"
+    ],
+    aspectRatio: {
+      width: 3,
+      height: 2
+    }
+  },
+  "/roster/Sebastian_Boyd/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Sophia_Lin/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  },
+  "/roster/Zander_Otavka/images/main.jpg": {
+    resolutions: [
+      "1200w",
+      "1800w",
+      "3600w"
+    ],
+    aspectRatio: {
+      width: 3,
+      height: 2
+    }
+  },
+  "/roster/Zander_Otavka/images/portfolio.jpg": {
+    resolutions: [
+      "1024w",
+      "2048w",
+      "683w"
+    ],
+    aspectRatio: {
+      width: 512,
+      height: 249
+    }
+  },
+  "/roster/Zander_Otavka/profile.jpg": {
+    resolutions: [
+      "1x",
+      "2x",
+      "3x"
+    ],
+    aspectRatio: {
+      width: 1,
+      height: 1
+    }
+  }
+};
